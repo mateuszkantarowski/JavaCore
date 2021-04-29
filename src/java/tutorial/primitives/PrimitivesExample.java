e8a3f1a4f6378e785ee447196fdd095aae43a06a
mateuszkantarowski <mateuszkantarowski@gmail.com>
2021-04-29 07:54:52 +0200

    primitives examples

---
+++ src/java/tutorial/primitives/PrimitivesExample.java
@@ -0,0 +1,14 @@
+package java.tutorial.primitives;
+
+public class PrimitivesExample {
+
+    /* Data Type	Default Value (for fields)      min                         max
+       byte	            0                           -128                        127
+       short	        0                           -32768                      32767
+       int	            0                           -2^31                       2^31 -1
+       long	            0L                          -2^63                       2^63 -1
+       float	        0.0f
+       double	        0.0d
+       char	            '\u0000'                    '\u0000' (or 0)             '\uffff' (or 65,535 inclusive)
+       boolean	        false */
+}
